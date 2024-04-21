c287f58f4e290eadb3300be22249ba8e95b127b5
oladakinjoye <oladakinjoye@gmail.com>
2024-04-21 11:53:12 -0700

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
-from flask import Flask
-from markupsafe import escape
+"""
+starts a Flask application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def display_hello():
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def desplay_hbnb():
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def display_cText(text):
-    text = text.replace("_", " ")
-    return "C %s" % (text)
-
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
